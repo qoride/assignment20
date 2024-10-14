9933f9fc609f9fd0d3e4dea401390650ccec4dce
qoride <qoride@gmail.com>
2024-10-14 19:24:34 +0000

    heap?

--- main.cpp
+++ main.cpp
@@ -4,20 +4,34 @@
 using namespace std;
 
 int main(){
-    int val;
+    int val, sum = 0;
 
-    cout << "Enter two values seperated by a space: ";
-    while(cin >> val){
-        if(cin.good())input.push_back(val);
-        if(input.size()>=2)break;
+    cout << "Enter a list of integers seperated by spaces: ";
+
+    int* num = new int;
+
+    while(true){
+        while(cin >> val){
+            if(cin.good()){
+                *num = val;
+                sum+=*num;
+            }
+            if(cin.peek() == '\n'){
+                cout << "\nEnter more values or type a string to end input: ";
+            }
+        }
+        if(cin.fail()){
+            cin.clear();
+            cout << "\nERROR: A value was not a number, ending...";
+            break;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            cin.clear();
+        }
     }
-    if(cin.fail()){
-        cin.clear();
-        cout << "\nERROR: A value was not accepted, try again: ";
-        input.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-        cin.clear();
-    }
+
+    delete num;
+
+    cout << "\n\nThe sum of all the inputs is " << sum << ".";
 
     return 0;
 }
